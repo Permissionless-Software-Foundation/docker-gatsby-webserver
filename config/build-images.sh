2412052344020a6c45397a1f589fb69a4fe9483e
Chris Troutner <chris.troutner@gmail.com>
2020-12-15 17:25:54 +0100

    Reconfirguring build-images.sh

--- config/build-images.sh
+++ config/build-images.sh
@@ -9,13 +9,14 @@ echo "Building containers for this repository: "$GATSBY_REPO
 # Re-build the Docker containers.
 cd ..
 /usr/local/bin/docker-compose down
+
+# Cleanup orphaned images.
+./config/cleanup-images.sh
+
 #/usr/local/bin/docker-compose build
 /usr/local/bin/docker-compose build --no-cache
 
 # Run the containers
-/usr/local/bin/docker-compose --project-name $DCNAME up -d 
-
-# Cleanup orphaned images.
-#./config/cleanup-images.sh
+/usr/local/bin/docker-compose --project-name $DCNAME up -d > ../$DCNAME-build.log
 
 echo "Docker containers have been re-built and are now running"
